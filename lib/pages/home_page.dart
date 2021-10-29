51e2ac0b01ba9636bf03fd791eb4e13c8a2172b7
Egemen <egemenongel@hotmail.com>
2021-10-29 18:20:07 +0300

    Refactor code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:to_do_list_with_provider/models/to_do_model.dart';
+import 'package:to_do_list_with_provider/utils/task_list_manager.dart';
 import 'package:to_do_list_with_provider/pages/list_title_page.dart';
 
 class HomePage extends StatefulWidget {
