d0cb13cf56621ad87742dea9e6b58d31be43080c
Egemen <egemenongel@hotmail.com>
2021-10-30 10:22:08 +0300

    Polish to do list UI

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -24,54 +24,54 @@ class _HomePageState extends State<HomePage> {
               MaterialPageRoute(builder: (context) => ListTitlePage()));
         },
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "TO DO LIST",
-              style: TextStyle(
-                  color: Colors.green[700],
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Row(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(left: 30),
-                  child: Column(
-                    children: [
-                      Text(
-                        "${_taskListManager.listTitle}",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 19),
-                      ),
-                      Container(
-                        width: 300,
-                        height: 400,
-                        child: ListView.builder(
-                            itemCount: _taskListManager.taskList.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              var task = _taskListManager.taskList[index];
-                              return ListTile(
-                                  title: Text(task.title),
-                                  trailing: Checkbox(
-                                    value: task.isCompleted,
-                                    onChanged: (value) {
-                                      _taskListManager.checkboxToggle(task);
-                                    },
-                                  ));
-                            }),
-                      ),
-                    ],
-                  ),
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            "TO DO LIST",
+            style: TextStyle(fontSize: 25),
+          ),
+        ),
+        automaticallyImplyLeading: false,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text(
+                "${_taskListManager.listTitle}",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 30),
+                child: Column(
+                  children: [
+                    Container(
+                      width: 300,
+                      height: 400,
+                      child: ListView.builder(
+                          itemCount: _taskListManager.taskList.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            var task = _taskListManager.taskList[index];
+                            return ListTile(
+                              title: Text(task.title),
+                              leading: Checkbox(
+                                value: task.isCompleted,
+                                onChanged: (value) {
+                                  _taskListManager.checkboxToggle(task);
+                                },
+                                activeColor: Colors.green,
+                              ),
+                            );
+                          }),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
