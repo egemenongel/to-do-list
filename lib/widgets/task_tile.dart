4630ff5303533146e4427c416780ed1314cc4ddf
Egemen <egemenongel@hotmail.com>
2021-10-31 14:34:55 +0300

    Unwrap the consumer
    Now, Task List Manager will be accessed from parent

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,30 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:to_do_list_with_provider/utils/task_list_manager.dart';
 
 class TaskTile extends StatelessWidget {
-  TaskTile(this.index);
+  TaskTile(this.index, this.taskListManager);
   final int? index;
+  final TaskListManager taskListManager;
   @override
   Widget build(BuildContext context) {
-    return Consumer<TaskListManager>(
-        builder: (context, taskListManager, child) {
-      var task = taskListManager.taskList[index!];
-      return ListTile(
-        title: Text(
-          task.title,
-          style: TextStyle(
-            decoration: task.isCompleted!
-                ? TextDecoration.lineThrough
-                : TextDecoration.none,
-          ),
+    var task = taskListManager.taskList[index!];
+    return ListTile(
+      title: Text(
+        task.title,
+        style: TextStyle(
+          decoration: task.isCompleted!
+              ? TextDecoration.lineThrough
+              : TextDecoration.none,
         ),
-        leading: Checkbox(
-          value: task.isCompleted,
-          onChanged: (value) => taskListManager.checkboxToggle(task),
-          activeColor: Colors.green,
+      ),
+      leading: Checkbox(
+        value: task.isCompleted,
+        onChanged: (value) => taskListManager.checkboxToggle(task),
+        activeColor: Colors.green,
+      ),
+      trailing: IconButton(
+        icon: Icon(
+          Icons.remove_circle_outline,
         ),
-      );
-    });
+        onPressed: () => taskListManager.removeTaskAt(index),
+      ),
+    );
   }
 }
