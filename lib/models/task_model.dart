e24f9a900083bf736178a0a5511cbbb30af2953c
Egemen <egemenongel@hotmail.com>
2021-11-02 20:45:33 +0300

    Add start and finish dates for tasks

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,9 +1,13 @@
 class TaskModel {
   String title;
   bool? isCompleted;
+  DateTime? startDate;
+  DateTime? finishDate;
 
   TaskModel({
     required this.title,
+    this.startDate,
+    this.finishDate,
     this.isCompleted,
   }) {
     this.title = title;
