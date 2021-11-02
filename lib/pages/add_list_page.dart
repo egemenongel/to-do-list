e1957597075c3990e9c9a84d0646a04d61b2a58b
Egemen <egemenongel@hotmail.com>
2021-11-02 22:14:00 +0300

    Add date form fields

--- lib/pages/add_list_page.dart
+++ lib/pages/add_list_page.dart
@@ -4,15 +4,23 @@ import 'package:to_do_list_with_provider/models/task_model.dart';
 import 'package:to_do_list_with_provider/utils/task_list_manager.dart';
 import 'package:to_do_list_with_provider/widgets/bottom_app_bar.dart';
 
-class AddListPage extends StatelessWidget {
+class AddListPage extends StatefulWidget {
   AddListPage({Key? key}) : super(key: key);
+
+  @override
+  State<AddListPage> createState() => _AddListPageState();
+}
+
+class _AddListPageState extends State<AddListPage> {
   final taskTitle = TextEditingController();
+  final startDate = TextEditingController();
+  final finishDate = TextEditingController();
   @override
   Widget build(BuildContext context) {
     var _taskListManager = Provider.of<TaskListManager>(context, listen: false);
     return Scaffold(
-      body: Center(
-          child: Column(
+      resizeToAvoidBottomInset: false,
+      body: Column(
         children: [
           SizedBox(
             height: 100,
@@ -27,19 +35,38 @@ class AddListPage extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 40),
             child: Form(
-              child: TextFormField(
-                controller: taskTitle,
-                decoration: InputDecoration(labelText: "New Task"),
-                autofocus: true,
-              ),
-            ),
+                child: Column(
+              children: [
+                TextFormField(
+                  controller: taskTitle,
+                  decoration: InputDecoration(labelText: "New Task"),
+                  autofocus: true,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Start Date"),
+                  controller: startDate,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Finish Date"),
+                  controller: finishDate,
+                )
+              ],
+            )),
           ),
           SizedBox(
             height: 15,
           ),
           ElevatedButton(
             onPressed: () {
-              TaskModel task = TaskModel(title: taskTitle.text);
+              TaskModel task = TaskModel(
+                title: taskTitle.text,
+              );
               _taskListManager.addTask(task);
               taskTitle.clear();
             },
@@ -56,6 +83,8 @@ class AddListPage extends StatelessWidget {
                           icon: Icon(Icons.remove_circle),
                           onPressed: () => taskListManager.removeTask(task),
                         ),
+                        subtitle: Text(
+                            "${task.startDate!.format(context).split(" ")[0]}"),
                         title: Row(
                           children: [
                             Text("${index + 1}. "),
@@ -68,7 +97,7 @@ class AddListPage extends StatelessWidget {
                     })),
           ),
         ],
-      )),
+      ),
       bottomNavigationBar: ListBottomBar(),
     );
   }
