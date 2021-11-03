a3b39fb7dcc246d24f38ee4b9530e5a226e7b73d
Egemen <egemenongel@hotmail.com>
2021-11-03 16:19:24 +0300

    Add firebase core package to the project

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,66 @@ import 'package:to_do_list_with_provider/theme/theme.dart';
 import 'package:to_do_list_with_provider/utils/task_list_manager.dart';
 import 'package:provider/provider.dart';
 import 'package:to_do_list_with_provider/pages/list_title_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => TaskListManager()),
-      ],
-      child: MyApp(),
-    ));
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => TaskListManager()),
+    ],
+    child: MyApp(),
+  ));
+}
+
+/// We are using a StatefulWidget such that we only create the [Future] once,
+/// no matter how many times our widget rebuild.
+/// If we used a [StatelessWidget], in the event where [App] is rebuilt, that
+/// would re-initialize FlutterFire and make our application re-enter loading state,
+/// which is undesired.
+class App extends StatefulWidget {
+  // Create the initialization Future outside of `build`:
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            builder: (context, child) => MediaQuery(
+              data:
+                  MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+              child: child!,
+            ),
+            title: 'Flutter Demo',
+            theme: MyTheme.theme,
+            home: ListTitlePage(),
+            debugShowCheckedModeBanner: false,
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return CircularProgressIndicator();
+      },
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
