3cc08929a2f93f65f817d2ded7e666c3a7389346
Egemen <egemenongel@hotmail.com>
2021-11-04 14:56:40 +0300

    Add initial form values

--- lib/widgets/edit_dialog.dart
+++ lib/widgets/edit_dialog.dart
@@ -1,17 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:to_do_list_with_provider/services/firestore.dart';
 import 'package:to_do_list_with_provider/widgets/task_form.dart';
 
 class EditDialog extends StatelessWidget {
   EditDialog({Key? key, required this.index}) : super(key: key);
   final index;
+
+  FireStoreService firestore = FireStoreService();
+
   final taskTitle = TextEditingController();
+
   final startTime = TextEditingController();
+
   final finishTime = TextEditingController();
+
   final _formKey = GlobalKey<FormState>();
+
   final duration = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    taskTitle.text = firestore.list1.doc("task1").id;
     return AlertDialog(
       shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(10),
@@ -23,19 +34,33 @@ class EditDialog extends StatelessWidget {
       ),
       content: Container(
         height: 250,
-        child: TaskForm(
-          formKey: _formKey,
-          taskTitle: taskTitle,
-          startTime: startTime,
-          finishTime: finishTime,
-          duration: duration,
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        child: StreamBuilder(
+          stream: firestore.orderById.snapshots(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (!snapshot.hasData) {
+              return CircularProgressIndicator();
+            }
+            //SET INITIAL VALUES
+            var task = snapshot.data.docs[index];
+            taskTitle.text = task["title"];
+            startTime.text = task["startTime"];
+            finishTime.text = task["finishTime"];
+            duration.text = task["duration"];
+            return TaskForm(
+              formKey: _formKey,
+              taskTitle: taskTitle,
+              startTime: startTime,
+              finishTime: finishTime,
+              duration: duration,
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+            );
+          },
         ),
       ),
       actions: [
         TextButton(
             onPressed: () {
-              FireStoreService firestore = FireStoreService();
               firestore.editTask(index, taskTitle.text, startTime.text,
                   finishTime.text, duration.text);
               Navigator.pop(context);
