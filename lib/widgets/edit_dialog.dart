570bb6a7fe23fbef1b5603cc5804eeac47c39a7f
Egemen <egemenongel@hotmail.com>
2021-11-04 13:07:36 +0300

    Create edit form

--- lib/widgets/edit_dialog.dart
+++ lib/widgets/edit_dialog.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list_with_provider/widgets/task_form.dart';
 
 class EditDialog extends StatelessWidget {
-  const EditDialog({Key? key}) : super(key: key);
-
+  EditDialog({Key? key}) : super(key: key);
+  final taskTitle = TextEditingController();
+  final startTime = TextEditingController();
+  final finishTime = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final durationController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text(
         "Edit Task",
       ),
+      content: TaskForm(
+        formKey: _formKey,
+        taskTitle: taskTitle,
+        startTime: startTime,
+        finishTime: finishTime,
+        duration: durationController,
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+      ),
       actions: [
         TextButton(onPressed: () {}, child: Text("Edit")),
-        TextButton(onPressed: () {}, child: Text("Cancel"))
+        TextButton(
+            onPressed: () => Navigator.pop(context), child: Text("Cancel"))
       ],
     );
   }
