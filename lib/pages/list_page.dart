1471afb902ec569066fa79c56e4928a41e4f7245
Egemen <egemenongel@hotmail.com>
2021-11-05 21:24:11 +0300

    Change tasks snapshot order

--- lib/pages/list_page.dart
+++ lib/pages/list_page.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:to_do_list_with_provider/services/firestore.dart';
 import 'package:to_do_list_with_provider/pages/list_title_page.dart';
+import 'package:to_do_list_with_provider/widgets/add_dialog.dart';
 import 'package:to_do_list_with_provider/widgets/task_tile.dart';
 
 class ListPage extends StatelessWidget {
@@ -9,116 +11,91 @@ class ListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var firestore = FireStoreService();
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            "TO DO LIST",
-            style: TextStyle(fontSize: 25),
+    return StreamBuilder(
+      stream: firestore.orderByTimestamp.snapshots(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return Scaffold(
+          appBar: AppBar(
+            title: Center(
+              child: Text(
+                "TO DO LIST",
+                style: TextStyle(fontSize: 25),
+              ),
+            ),
+            automaticallyImplyLeading: false,
           ),
-        ),
-        automaticallyImplyLeading: false,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 30,
-            ),
-            // Text(
-            //   "${context.read<TaskListManager>().listTitle}",
-            //   style: Theme.of(context).textTheme.headline2,
-            // ),
-            Expanded(
-                child: StreamBuilder(
-              stream: firestore.orderById.snapshots(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (!snapshot.hasData) {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                return ListView.separated(
-                  itemCount: snapshot.data.docs.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    var task = snapshot.data.docs[index];
-                    return TaskTile(
-                      index: index,
-                      taskTitle: task["title"],
-                      startTime: task["startTime"],
-                      finishTime: task["finishTime"],
-                      duration: task["duration"],
-                      isCompleted: task["isCompleted"],
-                      checkboxCallback: (checkboxState) =>
-                          firestore.checkboxToggle(task, checkboxState!),
-                      deleteCallback: () => firestore.deleteTask(task),
-                    );
-                  },
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 20,
+          body: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 30,
+                ),
+                // Text(
+                //   "${context.read<TaskListManager>().listTitle}",
+                //   style: Theme.of(context).textTheme.headline2,
+                // ),
+                Expanded(
+                  child: ListView.separated(
+                    itemCount: snapshot.data.docs.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      var task = snapshot.data.docs[index];
+                      return TaskTile(
+                        index: index,
+                        taskTitle: task["title"],
+                        startTime: task["startTime"],
+                        finishTime: task["finishTime"],
+                        duration: task["duration"],
+                        isCompleted: task["isCompleted"],
+                        checkboxCallback: (checkboxState) =>
+                            firestore.checkboxToggle(task, checkboxState!),
+                        deleteCallback: () => firestore.deleteTask(task),
+                      );
+                    },
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                    ),
+                    separatorBuilder: (BuildContext context, int index) {
+                      return Divider(
+                        height: 10,
+                        color: Colors.transparent,
+                      );
+                    },
                   ),
-                  separatorBuilder: (BuildContext context, int index) {
-                    return Divider(
-                      height: 10,
-                      color: Colors.transparent,
-                    );
+                ),
+              ],
+            ),
+          ),
+          persistentFooterButtons: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FloatingActionButton(
+                  child: Icon(Icons.add),
+                  onPressed: () {
+                    showDialog(
+                        context: context, builder: (context) => AddTask());
+                    // Add a task to the list
                   },
-                );
-              },
-            )),
-            Container(
-              color: Colors.purple[200],
-              height: 40,
-            ),
-            // Expanded(
-            //   child: Consumer<TaskListManager>(
-            //     builder: (_, taskListManager, __) => ListView.separated(
-            //       itemCount: taskListManager.listLength,
-            //       itemBuilder: (BuildContext context, int index) {
-            //         var task = taskListManager.taskList[index];
-            //         return TaskTile(
-            //           taskTitle: task.title,
-            //           isCompleted: task.isCompleted!,
-            //           startTime: task.startTime!,
-            //           finishTime: task.finishTime!,
-            //           duration: task.duration!,
-            //           checkboxCallback: (checkboxState) =>
-            //               taskListManager.checkboxToggle(task),
-            //         );
-            //       },
-            //       padding: EdgeInsets.symmetric(horizontal: 20),
-            //       separatorBuilder: (BuildContext context, int index) {
-            //         return Divider(
-            //           height: 10,
-            //           color: Colors.transparent,
-            //         );
-            //       },
-            //     ),
-            //   ),
-            // ),
+                ),
+                FloatingActionButton(
+                  child: Icon(Icons.add_box_outlined),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ListTitlePage()));
+                  },
+                ),
+              ],
+            )
           ],
-        ),
-      ),
-      persistentFooterButtons: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            FloatingActionButton(
-              child: Icon(Icons.add),
-              onPressed: () {
-                // Add a task to the list
-              },
-            ),
-            FloatingActionButton(
-              child: Icon(Icons.add_box_outlined),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ListTitlePage()));
-              },
-            ),
-          ],
-        )
-      ],
+        );
+      },
     );
   }
 }
