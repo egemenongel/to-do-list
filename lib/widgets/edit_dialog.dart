7294d153453a642a775cd8424e21bf8f74211fff
Egemen <egemenongel@hotmail.com>
2021-11-05 13:12:34 +0300

    Fix edit task method

--- lib/widgets/edit_dialog.dart
+++ lib/widgets/edit_dialog.dart
@@ -22,59 +22,56 @@ class EditDialog extends StatelessWidget {
   final duration = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-          side: BorderSide(
-            color: Colors.deepPurple,
-          )),
-      title: Text(
-        "Edit Task",
-      ),
-      content: Container(
-        height: 350,
-        child: StreamBuilder(
-          stream: firestore.orderById.snapshots(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (!snapshot.hasData) {
-              return Center(child: CircularProgressIndicator());
-            }
-            //SET INITIAL VALUES
-            var task = snapshot.data.docs[index];
-            taskTitle.text = task["title"];
-            startTime.text = task["startTime"];
-            finishTime.text = task["finishTime"];
-            duration.text = task["duration"];
-            return Column(
-              children: [
-                TaskForm(
-                  formKey: _formKey,
-                  taskTitle: taskTitle,
-                  startTime: startTime,
-                  finishTime: finishTime,
-                  duration: duration,
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                ),
-                TextButton(
+    return Container(
+      height: 350,
+      child: StreamBuilder(
+        stream: firestore.orderById.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          //SET INITIAL VALUES
+          var task = snapshot.data.docs[index];
+          taskTitle.text = task["title"];
+          startTime.text = task["startTime"];
+          finishTime.text = task["finishTime"];
+          duration.text = task["duration"];
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+                side: BorderSide(
+                  color: Colors.deepPurple,
+                )),
+            title: Text(
+              "Edit Task",
+            ),
+            content: TaskForm(
+              formKey: _formKey,
+              taskTitle: taskTitle,
+              startTime: startTime,
+              finishTime: finishTime,
+              duration: duration,
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+            ),
+            actions: [
+              TextButton(
                   onPressed: () {
                     firestore.editTask(task, taskTitle.text, startTime.text,
                         finishTime.text, duration.text);
                     Navigator.pop(context);
                   },
-                  child: Text("a"),
-                )
-              ],
-            );
-          },
-        ),
+                  child: Text("Edit")),
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Text("Cancel"))
+            ],
+            actionsAlignment: MainAxisAlignment.spaceBetween,
+          );
+        },
       ),
-      actions: [
-        // TextButton(onPressed: editCallback, child: Text("Edit")),
-        TextButton(
-            onPressed: () => Navigator.pop(context), child: Text("Cancel"))
-      ],
-      actionsAlignment: MainAxisAlignment.spaceBetween,
     );
   }
 }
