5fab97839d71fa4f2890ff74d4f3d68b1035d2f7
Egemen <egemenongel@hotmail.com>
2021-11-05 21:40:52 +0300

    Update model
    Updated the model so that, it can store timestamp now and it has to map and from map methods

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,9 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class TaskModel {
   String title;
   bool? isCompleted;
   String? startTime;
   String? finishTime;
   String? duration;
+  Timestamp? timeStamp;
 
   TaskModel({
     required this.title,
@@ -11,15 +14,40 @@ class TaskModel {
     this.finishTime,
     this.duration,
     this.isCompleted,
+    this.timeStamp,
   }) {
     this.title = title;
     this.isCompleted = false;
     this.startTime = startTime;
     this.finishTime = finishTime;
     this.duration = duration;
+    this.timeStamp = timeStamp;
   }
 
   void toggle() {
     isCompleted = !isCompleted!;
+  }
+
+  Map<String, dynamic> toMap() {
+    //model to map
+    return {
+      "title": title,
+      "isCompleted": isCompleted,
+      "startTime": startTime,
+      "finishTime": finishTime,
+      "duration": duration,
+      "timeStamp": timeStamp,
+    };
+  }
+
+  static TaskModel fromMap(Map<String, dynamic> map) {
+    return TaskModel(
+      title: map["title"],
+      isCompleted: map["isCompleted"],
+      startTime: map["startTime"],
+      finishTime: map["finishTime"],
+      duration: map["duration"],
+      timeStamp: map["timeStamp"],
+    );
   }
 }
