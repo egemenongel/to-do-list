bbafebd65c48679d400b899ef0df92c17d5183e2
Egemen <egemenongel@hotmail.com>
2021-11-08 20:31:20 +0300

    Change file names

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:to_do_list_with_provider/pages/lists_page.dart';
+import 'package:to_do_list_with_provider/pages/task_lists_page.dart';
 import 'package:to_do_list_with_provider/theme/theme.dart';
 import 'package:to_do_list_with_provider/utils/task_list_manager.dart';
 import 'package:provider/provider.dart';
@@ -40,7 +40,7 @@ class _MyAppState extends State<MyApp> {
             ),
             title: 'Flutter Demo',
             theme: MyTheme.theme,
-            home: ListsPage(),
+            home: TaskListsPage(),
             debugShowCheckedModeBanner: false,
           );
         }
